582dc80ae8321682c6be0846d389a315c895ac5e
Hemant Yadv <hemantyadavtech25@gmail.com>
2024-05-12 14:08:07 +0530

    feat: added the buld yml

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
-import { latin } from "next/font/google";
 
 const outfit = Outfit({
   subsets :["latin"],
